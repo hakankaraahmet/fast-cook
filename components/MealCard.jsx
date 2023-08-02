7f26202a3de25c118beb3cc1786788c27a312346
hakankaraahmet <karaahmethkn@gmail.com>
2023-08-02 22:28:50 +0200

    navbar is completed

--- components/MealCard.jsx
+++ components/MealCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MealCard = ({ item }) => {
   return (
 
-      <div className="flex flex-col justify-between mx-auto p-9 bg-white max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
+      <div className="flex flex-col justify-between mx-auto p-9 bg-gradient-mainTheme max-w-sm rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition duration-300">
         <img
           className="rounded-xl"
           src={item?.image}
@@ -14,7 +14,7 @@ const MealCard = ({ item }) => {
             <h1 className="mt-5 text-xl font-semibold">{item?.title}</h1>
           </div>
           <div>
-            <button className="mt-5 text-white text-md font-semibold bg-green-400 py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">
+            <button className="mt-5 text-mainButtonText text-md font-semibold bg-mainButton py-2 px-4 rounded-lg shadow-md hover:shadow-lg transition duration-500 transform-gpu hover:scale-110 ">
               Receipt
             </button>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        mainText: '#fff',
+        mainButton: '#f9fafb',
+        mainButtonText : '#701a75'
+      },
     },
   },
   plugins: [],
