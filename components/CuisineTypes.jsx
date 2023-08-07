f9320444613eb01b9b6236ac4a6402108f6b3dd5
hakankaraahmet <karaahmethkn@gmail.com>
2023-08-07 23:30:21 +0200

    selection funcionality is added

--- components/CuisineTypes.jsx
+++ components/CuisineTypes.jsx
@@ -1,33 +1,49 @@
-import React from 'react'
-import {cuisines} from '../constants/cuisines'
+import React, { useEffect, useState } from "react";
+import { cuisines } from "../constants/cuisines";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchRecipe , setIsRecipeSelected } from '../redux/features/recipeSlice';
+import {
+  fetchRecipe,
+  setIsRecipeSelected,
+} from "../redux/features/recipeSlice";
+import Image from "next/image";
 const CuisineTypes = () => {
-   const dispatch = useDispatch();
-   const { recipe, status, error , isRecipeSelected } = useSelector((state) => state.recipe);
+  const [cuisineList, setCuisineList] = useState(cuisines);
+  const dispatch = useDispatch();
+  const { recipe, status, error, isRecipeSelected } = useSelector(
+    (state) => state.recipe
+  );
 
-   console.log('isRecipeSelected :>> ', isRecipeSelected);
+  const selectRecipe = (cuisineType) => {
+    // dispatch(fetchRecipe(cuisineType.value));
+    // dispatch(setIsRecipeSelected(true));
+    const updatedCuisines = cuisineList.map((item) => {
+      if (item.id === cuisineType.id)
+        return {
+          ...item,
+          isSelected: !item.isSelected
+        };
+      return item;
+    });
+    setCuisineList(updatedCuisines);
+  };
 
-   const selectRecipe = (cuisineType) => {
-    dispatch(fetchRecipe(cuisineType.value))
-    dispatch(setIsRecipeSelected(true))
-   }
-
-   console.log('recipe :>> ', recipe);
 
   return (
-    <div className=' grid  md:grid-cols-2 lg:grid-cols-3'>
-      {cuisines?.map((cuisineType) => (
+    <div className="grid md:grid-cols-2 lg:grid-cols-3">
+      {cuisineList?.map((cuisineType) => (
         <span
-        key={cuisineType.id}
-        onClick={() => selectRecipe(cuisineType)}
-        className="text-white mx-8 flex  items-center text-sm  py-2 px-8 bg-mainButtonText rounded-lg my-4"
-      >
-        {cuisineType.value}
-      </span>
+          key={cuisineType.id}
+          onClick={() => selectRecipe(cuisineType)}
+          className={ `flex items-center justify-between cursor-pointer px-4 py-2 mx-8 my-4 text-sm  rounded-lg   ${cuisineType.isSelected ? 'bg-mainButtonText text-white' : 'bg-white text-mainButtonText'}`}
+        >
+          {cuisineType.value}
+          <span className="flex items-center justify-center scale-125 ">
+            {cuisineType.icon}
+          </span>
+        </span>
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default CuisineTypes
+export default CuisineTypes;
